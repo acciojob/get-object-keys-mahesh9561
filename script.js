1e8653d5448487b4ae17e6fa6c6b087ee7b81f51
Mahesh Pathak <53071108+mahesh9561@users.noreply.github.com>
2023-12-25 08:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// Create a object
+let student = {
+	name:"Mahesh"
+}
+Object.prototype.getKeys = function() {
+	return Object.keys(this);
+}
+
+const tryObj = student.getKeys();
+console.log(tryObj);
